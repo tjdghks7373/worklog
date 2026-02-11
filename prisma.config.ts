8b5618dc645199cf644eae1ea01ac20380783d66
kim <tjdghks7373@naver.com>
2026-02-11 15:42:42 +0900

    SUPABASE 적용 2

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
+    // process.env에서 직접 가져오되, 타입스크립트 에러 방지를 위해 as string 추가
+    url: process.env.DATABASE_URL as string,
   },
 });
